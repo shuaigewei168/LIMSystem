7bd0a61d969e8abfb4b924aac698bb3382f17678
jiawei <jiawei.com>
2017-02-13 23:33:45 +0800

    添加mysql的php代码

---
+++ api/inc.php
@@ -0,0 +1,14 @@
+<?php
+/***********************
+函数：inc.php
+输入：
+输出：
+
+功能：配置文件
+************************/
+//数据库配置
+define("LOCALHOST", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "linjiawei");
+define("TABLE", "limsystem");
+?>
---
+++ api/mysql.php
@@ -0,0 +1,23 @@
+<?php
+/***********************
+函数：mysql.php
+输入：
+输出：
+
+功能：配置数据库
+************************/
+include_once('inc.php');
+//创建对象并打开连接，最后一个参数是选择的数据库名称
+$mysqli = new mysqli(LOCALHOST,USERNAME,PASSWORD,TABLE);
+//检查连接是否成功
+if (mysqli_connect_errno()){
+	//注意mysqli_connect_error()新特性
+	die('Unable to connect!'). mysqli_connect_error();
+}
+// $sql = "select * from vol_msg";
+//执行sql语句，完全面向对象的
+// $result = $mysqli->query($sql);
+// while($row = $result->fetch_array()){
+// 	echo $row[0];
+// }
+?>
