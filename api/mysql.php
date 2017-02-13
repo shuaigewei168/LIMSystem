8c09b091d9d7f4787e6064ea385e096d84885209
jiawei <jiawei.com>
2017-02-13 23:34:49 +0800

    添加mysql的php代码

--- api/mysql.php
+++ api/mysql.php
@@ -14,7 +14,7 @@ if (mysqli_connect_errno()){
 	//注意mysqli_connect_error()新特性
 	die('Unable to connect!'). mysqli_connect_error();
 }
-// $sql = "select * from vol_msg";
+$sql = "select * from vol_msg";
 //执行sql语句，完全面向对象的
 // $result = $mysqli->query($sql);
 // while($row = $result->fetch_array()){
